bb564a05cf8eb6a94e07ce7c3caf544c74f1a9e3
DhrumilaShah <dhrumilshah2495@gmail.com>
2019-04-26 20:05:44 -0400

    data mining

--- src/Crawler.java
+++ src/Crawler.java
@@ -11,23 +11,39 @@ import java.util.Set;
 
 public class Crawler {
 
+
+	public String getDescription(String link) {
+		try {
+			Document doc = Jsoup.connect(link).get();
+			String keywords = doc.select("meta[name=keywords]").first().attr("content");
+			System.out.println(keywords);
+			return doc.select("meta[name=description]").get(0).attr("content");
+		}catch(Exception e) {
+			//e.printStackTrace();
+			//System.err.println("Problem in getDescription: "+e.getMessage());
+			return "";
+		}
+	}
+
 	public Set<String> getLinks(String website,String domain) throws IOException, URISyntaxException {
 		try {
-			
+
 			Elements rootLinks = Jsoup.connect(website).get().select("a[href]");
 
 			Set <String> allLinks = new HashSet <String>();
 
 			for (Element link : rootLinks) {
-				String l = getDomainName(link.attr("abs:href")); 
-				if( l != null && !isSocialMedia(l) && !domain.contains(l) && !l.contains(domain))
-					allLinks.add(l);
+				String lnk =link.attr("abs:href");
+				String dom = getDomainName(lnk); 
+				if( dom != null && !isSocialMedia(dom) && !domain.contains(dom) && !dom.contains(domain))
+					allLinks.add(lnk);
 			}
 			return allLinks;
 		}catch(Exception e) {
-			System.err.println(e.getMessage());
+			//System.err.println("Problem in getLinks: "+e.getMessage());
+			return null;
 		}
-		return null;
+		
 	}
 
 	public String getDomainName(String url) {
@@ -36,20 +52,18 @@ public class Crawler {
 			String domain = uri.getHost();
 			return domain.startsWith("www.") ? domain.substring(4) : domain;
 		}catch(Exception e) {
-			System.err.println(e.getMessage());
+			//System.err.println("Problem in getDomainName: "+e.getMessage());
+			return null;
 		}
-		return null;
+		
 	}
 
 	private boolean isSocialMedia (String domain) {
-
 		String[] socialMedia = {"stumbleupon.com","youtube.com","twitter.com","linkedin.com", "reddit.com","facebook.com",
 				"pinterest.com","instagram.com","tumblr.com","flickr.com","snapchat.com","whatsapp.com"};
-
 		for(String s : socialMedia) 
 			if(s.equalsIgnoreCase(domain)) 
 				return true;
-
 		return false;
 	}
 
--- src/Hits.java
+++ src/Hits.java
@@ -1,55 +1,167 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
 public class Hits {
+
 	static FileReader fr;
 	static Map<Integer,Integer[]> graph;
+	static Map<String,Integer> conversionGraph;
 	static int[][] adjMtrx;
 	static int vertices;
 	static double[] hub;
 	static double[] auth;
+	static double[] prevHub;
+	static double[] prevAuth;
 	static final double initValue = 1.0;
+	static double errRate = 100000;
+	static int numOfPages = 5;
+	static ArrayList<Integer> topAuthIndex; 
+	static ArrayList<Integer> topHubIndex;
+	static int iterations = 1500;
+	static Crawler crwl;
 
 	public static void main(String[] args) throws Exception {
+
 		graph = new HashMap<Integer,Integer[]>();
 
-		fr = new FileReader("machine_learning_root_set");
+		fr = new FileReader("big_data_analytics_root_set");
 
-		System.out.println("Building graph...");
+		crwl = new Crawler();
+
+		//System.out.println("Building graph...");
 
 		buildGraph();
-
-		//		System.out.println(graph.size());	
-		//		Iterator it = graph.entrySet().iterator();
-		//		while (it.hasNext()) {
-		//			Map.Entry pair = (Map.Entry)it.next();
-		//			//System.out.println(pair.getKey() + " = " + pair.getValue());
-		//			Integer[] i = (Integer[])pair.getValue();
-		//			for(int k=0; k<i.length; k++) {
-		//				System.out.println("--"+(int)pair.getKey()+"--"+(int)i[k]);
-		//			}
-		//		}
 
 		initMtrx();
 		initHubAuth();
 
-		System.out.println(adjMtrx.length);	
-		printMtrx();
+		for(int i=0; i<iterations; i++){
+			prevHub = hub;
+			prevAuth = auth;
 
+			auth = computeAuth();
+			hub = computeHub();
 
+			double[] dash=computeDash(hub,auth);//0=hub, 1=auth
 
-		//loop{
-		//print hub and auth
+			auth = scaleMtrx(dash[1],auth);
+			hub = scaleMtrx(dash[0],hub);
 
-		computeAuth();
-		computeHub();
+			if(didItConverge()) break;
+		}
 
+		topAuthIndex = new ArrayList<Integer> (numOfPages); 
+		topHubIndex = new ArrayList<Integer> (numOfPages); 
 
+		setTopAuthIndex();
+		setTopHubIndex();
 
-		//}
+		System.out.println("--------------------Authority--------------------");
+		System.out.println();
+		for(int l : topAuthIndex) {
+			
+			String lnk = getLink(l);
+			System.out.println(lnk);
+			System.out.println(crwl.getDescription(lnk));
+			System.out.println("Authority : "+auth[l] +"     Hub : "+hub[l]);
+			System.out.println();
 
+		}
+		System.out.println("--------------------Hub--------------------");
+		System.out.println();
+		for(int l : topHubIndex) {
+			
+			String lnk = getLink(l);
+			System.out.println(lnk);
+			System.out.println(crwl.getDescription(lnk));
+			System.out.println("Authority : "+auth[l] +"     Hub : "+hub[l]);
+			System.out.println();
+		}
+
+	}
+
+	public static String getLink(int index) {
+
+		Iterator it = conversionGraph.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			int i = (int)pair.getValue();
+			//System.out.println(i+"-->>>"+index);
+			if(i==index)
+				return (String)pair.getKey();
+		}
+		return null;
+	}
+
+	public static void setTopAuthIndex(){ 
+		for(int k=0; k<numOfPages; k++) {
+			double maxValue = -1; 
+			int index = -1;
+			for(int i=0; i<auth.length; i++){ 
+				if(!topAuthIndex.contains(i)) {
+					if(auth[i] > maxValue){ 
+						maxValue = auth[i]; 
+						index = i;
+					}
+				} 
+			} 
+			if(index == -1 ) break;
+			topAuthIndex.add(k,index);
+		}
+	}
+
+	public static void setTopHubIndex(){ 
+		for(int k=0; k<numOfPages; k++) {
+			double maxValue = -1; 
+			int index = -1;
+			for(int i=0; i<hub.length; i++){ 
+				if(!topHubIndex.contains(i)) {
+					if(hub[i] > maxValue){ 
+						maxValue = hub[i]; 
+						index = i;
+					}
+				} 
+			}
+			if(index == -1 ) break;
+			topHubIndex.add(k,index);
+		}
+	}
+
+	static boolean didItConverge() {
+		//errRate = Math.pow(10, (iter * -1));
+		for (int i = 0; i < auth.length; i++) {
+
+			if ((int)Math.floor(auth[i] * errRate) != (int)Math.floor(prevAuth[i] * errRate) || 
+					(int)Math.floor(hub[i] * errRate) != (int)Math.floor(prevHub[i] * errRate)) 
+				return false;
+		}
+		//System.out.println("Converged!");
+		return true;
+	}
+
+	static double[] scaleMtrx(double num, double[] mtrx) {
+		int size = mtrx.length;
+		double[] temp = new double[size];
+		for(int i=0; i<size; i++) { 
+			temp[i] = mtrx[i] / num;
+			if(Double.isNaN(temp[i]) || Double.isInfinite(temp[i]))	temp[i] = 0.0;
+		}
+		return temp;
+	}
+
+	static double[] computeDash(double[] u, double[] v) {
+		double sum1 = 0.0;
+		double sum2 = 0.0;
+		for(int i=0; i<hub.length; i++) {
+			sum1 += (double)Math.pow(hub[i],2);
+			sum2 += (auth[i] * auth[i]);
+		}
+		return new double[] {Math.sqrt(sum1),Math.sqrt(sum2)};
 	}
 
 	static double[] computeAuth() {
@@ -66,7 +178,7 @@ public class Hits {
 		}
 		return temp;
 	}
-	
+
 	static double[] computeHub() {
 		double sum;
 		double[] temp = new double[hub.length];
@@ -81,10 +193,10 @@ public class Hits {
 		}	
 		return temp;
 	}
-	
-
 
 	static void initHubAuth() {
+		hub = new double[vertices];
+		auth = new double[vertices];
 		for(int j=0; j<vertices; j++) {
 			hub[j] = initValue;
 			auth[j] = initValue;
@@ -99,7 +211,6 @@ public class Hits {
 		}
 	}
 
-
 	static void initMtrx(){
 		try{
 			adjMtrx = new int[vertices][vertices];
@@ -107,9 +218,11 @@ public class Hits {
 			while (it.hasNext()) {
 				Map.Entry pair = (Map.Entry)it.next();
 				Integer[] i = (Integer[])pair.getValue();
+				int key = (int)pair.getKey();
+				//System.out.println(vertices+"--"+key+"--"+i.length);
 				for(int k=0; k<i.length; k++) {
-					adjMtrx[(int)pair.getKey()][(int)i[k]] = 1;
-					System.out.println((int)pair.getKey()+"--"+(int)i[k]);
+					//System.out.println("--"+(int)i[k]);
+					adjMtrx[key][(int)i[k]] = 1;
 				}
 			}
 		}catch(Exception e) {
@@ -117,31 +230,35 @@ public class Hits {
 		}
 	}
 
-
-
 	static void buildGraph(){
 		try {
-			Crawler crwl = new Crawler();
+
 			//Map<Integer,Integer[]> graph = new HashMap<Integer,Integer[]>();
 			Map<String, Set<String>> allLinks = new HashMap<String, Set<String>>();
-			Map<String,Integer> conversionGraph = new HashMap<String,Integer>();
+			conversionGraph = new HashMap<String,Integer>();
 
 			int counter = 0;
 			String website = null;
 
 			while((website=fr.getNextValue()) != null) {
 				String domain = crwl.getDomainName(website);
-				if(domain!=null) {
-					conversionGraph.put(domain,counter++);
+				if(domain!=null && website!=null) {
+					//System.out.println(website+"--"+counter);
+					conversionGraph.put(website,counter++);
 					Set<String> links = crwl.getLinks(website,domain);
-					allLinks.put(domain,links);
-					for(String s : links) { 
-						conversionGraph.put(s,counter++);
+					if(links!=null) {
+						allLinks.put(website,links);
+						for(String s : links) { 
+							//if(s!=null) {
+							//System.out.println(s+"--"+counter);
+							if(!conversionGraph.containsKey(s))
+								conversionGraph.put(s,counter++);
+							//}
+						}
 					}
 				}
 			}
-			vertices = counter-1;
-			//System.out.println("-->>"+vertices);
+			vertices = counter;
 
 			Iterator it = allLinks.entrySet().iterator();
 			while (it.hasNext()) {
